316a9570cc93e732172e527b33245a69ee30caed
buahduku33 <36914647+buahduku33@users.noreply.github.com>
2018-04-11 18:13:14 +0700

    Add files via upload

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject enemy;
+    public Transform[] location;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(enemySpawner());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public IEnumerator enemySpawner()
+    {
+        yield return new WaitForSeconds(Random.Range(1, 5));
+        Instantiate(enemy,location[Random.Range(0,5)].position,Quaternion.identity);
+        StartCoroutine(enemySpawner());
+    }
+}
